c38ab295e8622d0ab42c694fa16d100b4dc0c550
xingchentye <xingchenawa@qq.com>
2024-07-31 22:13:02 +0800

    📦 适配PyPi打包格式

---
+++ README.md
@@ -0,0 +1,7 @@
+## PythonSqliteEasyCtrl
+
+PythonSqliteEasyCtrl是一个将数据库操作语句封装成操作类的库，它可以使操作Sqlite3数据库更加方便。
+
+PythonSqliteEasyCtrl还支持数据库信息加密功能
+
+该库目前为[`ToolDelta`](https://github.com/ToolDelta/ToolDelta)服务
---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools #导入setuptools打包工具
+ 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("version", "r", encoding="utf-8") as f:
+    version = f.read().strip()
+ 
+setuptools.setup(
+    name="Python-Sqlite3-EasyCtrl",
+    version=version,
+    author="xingchen",
+    author_email="xingchenawa@qq.com",
+    description="一个可以更方便的操作SQLite数据的Python库",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xingchentye/Python-Sqlite3-EasyCtrl",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
